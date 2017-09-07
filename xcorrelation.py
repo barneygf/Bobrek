01be9672ba30f415bb9285aa277281df55c7059c
rtape <czarny@min-pan.krakow.pl>
2017-09-07 14:49:05 +0200

    sdasdasd

---
+++ xcorrelation.py
@@ -0,0 +1,10 @@
+"""
+contains methods of correltation:
+xcorrelation function = cross correlatio
+xcoherence function = cross coherence
+xdeconv function= deconvowlution
+
+
+@author: R_Czarny
+"""
+print(0)
